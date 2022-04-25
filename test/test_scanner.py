8ac8d3645db8db25571dbfc334db4511c55bf68b
Restyled.io <commits@restyled.io>
2022-04-25 17:18:16 +0000

    Restyled by autopep8

--- test/test_scanner.py
+++ test/test_scanner.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     test_samples = os.listdir(TEST_SAMPLE_DIR_ABS)
     for no, test_sample in enumerate(test_samples):
         print(f"[{no + 1}/{len(test_samples)}] Running on sample {test_sample}...")
-        process = subprocess.Popen(f"{EXE_DIR_ABS} {TEST_SAMPLE_DIR_ABS}\\{test_sample}", shell=True)
+        process = subprocess.Popen(
+            f"{EXE_DIR_ABS} {TEST_SAMPLE_DIR_ABS}\\{test_sample}", shell=True)
         process.wait()
         print("\n")
