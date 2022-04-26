7aac8a97433ef81b8f0a6e4360ff6f9bdb532ebd
NPU-Franklin <1109797240@qq.com>
2022-04-27 02:21:21 +0800

    Add parser function:sparkles:
    Signed-off-by: NPU-Franklin <1109797240@qq.com>

---
+++ test/test_parser_windows.py
@@ -0,0 +1,12 @@
+import subprocess
+
+TEST_SAMPLE_ABS = "D:\\Projects\Compiler\\test\\test_sample\\000_main.sy"
+EXE_DIR_ABS = "D:\\Projects\\Compiler\\cmake-build-debug\\Compiler.exe"
+
+if __name__ == "__main__":
+    print(
+        f"Running on sample 000_main.sy..."
+    )
+    process = subprocess.Popen(
+        f"{EXE_DIR_ABS} {TEST_SAMPLE_ABS}", shell=True)
+    process.wait()
