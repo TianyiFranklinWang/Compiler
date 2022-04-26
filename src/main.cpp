ccb2fcd19decc9f0043b843cbd8f7847c9e29c41
Restyled.io <commits@restyled.io>
2022-04-26 18:34:36 +0000

    Restyled by clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 // Created by NPU-Franklin on 2022/4/27.
 //
 
+#include <cassert>
 #include <iostream>
 #include <memory>
-#include <cassert>
 
 using namespace std;
 
@@ -13,16 +13,16 @@ extern FILE *yyin;
 extern int yyparse(unique_ptr<string> &ast);
 
 int main(int argc, const char *argv[]) {
-    assert(argc == 2);
-    auto input = argv[1];
+  assert(argc == 2);
+  auto input = argv[1];
 
-    yyin = fopen(input, "r");
-    assert(yyin);
+  yyin = fopen(input, "r");
+  assert(yyin);
 
-    unique_ptr<string> ast;
-    auto ret = yyparse(ast);
-    assert(!ret);
-    cout << *ast << endl;
+  unique_ptr<string> ast;
+  auto ret = yyparse(ast);
+  assert(!ret);
+  cout << *ast << endl;
 
-    return 0;
+  return 0;
 }
