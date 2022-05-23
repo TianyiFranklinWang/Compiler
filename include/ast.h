bd4873740d0396ec8972736b4dde9ccd4aa1df90
Restyled.io <commits@restyled.io>
2022-05-23 13:09:02 +0000

    Restyled by clang-format

--- include/ast.h
+++ include/ast.h
@@ -12,54 +12,54 @@ namespace ast {
 // Base class for all AST
 class BaseAST {
 public:
-    virtual ~BaseAST() = default;
+  virtual ~BaseAST() = default;
 
-    virtual void Dump() const = 0;
+  virtual void Dump() const = 0;
 };
 
 class CompUnitAST : public BaseAST {
 public:
-    std::unique_ptr<BaseAST> func_def;
+  std::unique_ptr<BaseAST> func_def;
 
-    void Dump() const override;
+  void Dump() const override;
 };
 
 class FuncDefAST : public BaseAST {
 public:
-    std::unique_ptr<BaseAST> func_type;
-    std::string ident;
-    std::unique_ptr<BaseAST> block;
+  std::unique_ptr<BaseAST> func_type;
+  std::string ident;
+  std::unique_ptr<BaseAST> block;
 
-    void Dump() const override;
+  void Dump() const override;
 };
 
 class FuncTypeAST : public BaseAST {
 public:
-    std::string type;
+  std::string type;
 
-    void Dump() const override;
+  void Dump() const override;
 };
 
 class BlockAST : public BaseAST {
 public:
-    std::unique_ptr<BaseAST> stmt;
+  std::unique_ptr<BaseAST> stmt;
 
-    void Dump() const override;
+  void Dump() const override;
 };
 
 class StmtAST : public BaseAST {
 public:
-    std::unique_ptr<BaseAST> number;
+  std::unique_ptr<BaseAST> number;
 
-    void Dump() const override;
+  void Dump() const override;
 };
 
 class NumberAST : public BaseAST {
 public:
-    int value;
+  int value;
 
-    void Dump() const override;
+  void Dump() const override;
 };
-}
+} // namespace ast
 
-#endif //COMPILER_AST_H
+#endif // COMPILER_AST_H
