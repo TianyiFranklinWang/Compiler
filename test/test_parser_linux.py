8f30da715a3d0197a4ddd03ac1248f5144ffd08a
NPU-Franklin <1109797240@qq.com>
2022-05-23 20:41:36 +0800

    Add sudo permission:construction_worker:
    Signed-off-by: NPU-Franklin <1109797240@qq.com>

--- test/test_parser_linux.py
+++ test/test_parser_linux.py
@@ -1,9 +1,9 @@
 import subprocess
 
 TEST_SAMPLE_ABS = "/home/circleci/project/test/test_sample/000_main.sy"
-ELF_DIR_ABS = "/home/circleci/project/cmake-build-debug"
+ELF_DIR_ABS = "/home/circleci/project/cmake-build-debug/Compiler"
 
 if __name__ == "__main__":
     print(f"Running on sample 000_main.sy...")
-    process = subprocess.Popen(f"{ELF_DIR_ABS} {TEST_SAMPLE_ABS}", shell=True)
+    process = subprocess.Popen(f"sudo {ELF_DIR_ABS} {TEST_SAMPLE_ABS}", shell=True)
     process.wait()
